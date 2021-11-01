f72a42ac4a7ecb1a79cec3b23a2d83116f9aa7b4
askwuxue <972523419@qq.com>
2021-11-02 00:03:16 +0800

    refactor: use prettier refactor

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="nav">
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
+    |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
