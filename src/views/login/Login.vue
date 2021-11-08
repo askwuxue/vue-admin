ec30b6a0afc9267b2f1c18009fcc0bb7670d6545
askwuxue <972523419@qq.com>
2021-11-08 23:52:37 +0800

    feat: create login page

---
+++ src/views/login/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">login</div>
+</template>
+
+<script lang="ts">
+import { defineComponent, SetupContext } from 'vue'
+export default defineComponent({
+  name: '',
+  props: {},
+  components: {},
+  setup(props, ctx: SetupContext) {
+    return {}
+  },
+})
+</script>
+
+<style scoped lang="less"></style>
