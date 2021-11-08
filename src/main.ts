5b8036dabc2210812b75bd53c71c2af5221d23f6
askwuxue <972523419@qq.com>
2021-11-08 23:49:25 +0800

    style: import normalize.css

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import register from './global/register'
+import 'normalize.css'
 import 'element-plus/dist/index.css'
 // 按需加载组件的基础样式
 import 'element-plus/theme-chalk/base.css'
