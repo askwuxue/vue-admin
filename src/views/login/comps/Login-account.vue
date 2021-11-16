0e067d5697b001aa14771be3a6be076d45dfb354
askwuxue <972523419@qq.com>
2021-11-16 22:59:00 +0800

    feat: component split

---
+++ src/views/login/comps/Login-account.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="login-account">
+    <el-form
+      :label-position="labelPosition"
+      label-width="100px"
+      :model="account"
+      :rules="rules"
+      ref="formRef"
+    >
+      <el-form-item label="账号" prop="name">
+        <el-input v-model="account.name"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="pass">
+        <el-input v-model="account.pass"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { ElForm } from 'element-plus/lib/components'
+import { defineComponent, reactive, ref, SetupContext } from 'vue'
+import { rules } from '../config/login-account'
+export default defineComponent({
+  name: '',
+  props: {},
+  components: {},
+  setup(props, ctx: SetupContext) {
+    const account = reactive({
+      name: '',
+      pass: '',
+    })
+    const labelPosition = 'left'
+    const formRef = ref<InstanceType<typeof ElForm>>()
+    // 提交验证
+    const loginAction = () => {
+      formRef.value?.validate((valid) => {
+        console.log('valid: ', valid)
+        console.log('account 登录验证')
+      })
+    }
+    return {
+      account,
+      labelPosition,
+      rules,
+      formRef,
+      loginAction,
+    }
+  },
+})
+</script>
+
+<style scoped lang="less">
+// .password {
+//   margin-top: 20px;
+// }
+</style>
---
+++ src/views/login/comps/Login-phone.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="login-phone">
+    <el-form
+      :label-position="labelPosition"
+      label-width="100px"
+      :model="info"
+      :rules="rules"
+      ref="formRef"
+    >
+      <el-form-item label="手机号" prop="phone">
+        <el-input v-model="info.phone"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="pass">
+        <el-input v-model="info.pass"></el-input>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script lang="ts">
+import { ElForm } from 'element-plus'
+import { defineComponent, reactive, ref, SetupContext } from 'vue'
+import { rules } from '../config/login-phone'
+export default defineComponent({
+  name: '',
+  props: {},
+  components: {},
+  setup(props, ctx: SetupContext) {
+    const labelPosition = 'left'
+    const info = reactive({
+      phone: '',
+      pass: '',
+    })
+    // 表单ref
+    const formRef = ref<InstanceType<typeof ElForm>>()
+    // 表单验证
+    const loginAction = () => {
+      formRef.value?.validate((valid) => {
+        console.log('valid: ', valid)
+        console.log('真正的执行逻辑')
+      })
+    }
+
+    return {
+      labelPosition,
+      info,
+      rules,
+      formRef,
+      loginAction,
+    }
+  },
+})
+</script>
+
+<style scoped lang="less">
+// .password {
+//   margin-top: 20px;
+// }
+</style>
