dacb166f635c741f786dfdf0de4152ee82f7a1e9
askwuxue <972523419@qq.com>
2021-11-16 22:28:00 +0800

    feat: import element-plus components

--- src/global/element-component.ts
+++ src/global/element-component.ts
@@ -6,6 +6,20 @@ import {
   ElTabPane,
   ElCalendar,
   ElIcon,
+  ElInput,
+  ElForm,
+  ElFormItem,
 } from 'element-plus/lib/components'
 
-export default [ElButton, ElLink, ElRate, ElTabs, ElTabPane, ElCalendar, ElIcon]
+export default [
+  ElButton,
+  ElLink,
+  ElRate,
+  ElTabs,
+  ElTabPane,
+  ElCalendar,
+  ElIcon,
+  ElInput,
+  ElForm,
+  ElFormItem,
+]
