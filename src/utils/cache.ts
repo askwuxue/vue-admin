2f55077eaa8a627f91bf888f54d0053f2e52777b
askwuxue <972523419@qq.com>
2021-11-17 23:56:56 +0800

    feat: create utils folder and cache file

---
+++ src/utils/cache.ts
@@ -0,0 +1,22 @@
+class LocalCache {
+  setCache(key: string, value: any) {
+    window.localStorage.setItem(key, JSON.stringify(value))
+  }
+
+  getCache(key: string) {
+    const value = window.localStorage.getItem(key)
+    if (value) {
+      return JSON.parse(value)
+    }
+  }
+
+  deleteCache(key: string) {
+    window.localStorage.removeItem(key)
+  }
+
+  clearAll() {
+    window.localStorage.clear()
+  }
+}
+
+export default new LocalCache()
