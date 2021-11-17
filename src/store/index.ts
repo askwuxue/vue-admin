5cfae8649f9ad879902b56431cad926533ea9455
askwuxue <972523419@qq.com>
2021-11-18 00:00:26 +0800

    feat: split store's login module

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,17 @@
 import { createStore } from 'vuex'
+import { IRootState } from './types'
+import loginModule from './login/login'
 
-export default createStore({
-  state: {},
+export default createStore<IRootState>({
+  state() {
+    return {
+      name: '',
+      age: 0,
+    }
+  },
   mutations: {},
   actions: {},
-  modules: {},
+  modules: {
+    loginModule,
+  },
 })
