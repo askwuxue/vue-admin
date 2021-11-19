50b3f6cf9812b29eff9a1e0dcf0747df8a6da1bc
askwuxue <972523419@qq.com>
2021-11-19 23:57:14 +0800

    build: config devServer's proxy

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,38 @@
+// const path = require('path')
+
 module.exports = {
+  // 1.配置方式一: CLI提供的属性
+  outputDir: './build',
+  // publicPath: './',
+  // devServer: {
+  //   proxy: {
+  //     [process.env.VUE_APP_BASE_API]: {
+  //       target: 'http://152.136.185.210:5000',
+  //       pathRewrite: {
+  //         '^/api': '',
+  //       },
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
+  // 2.配置方式二: 和webpack属性完全一致, 最后会进行合并
   configureWebpack: {
-    plugins: [
-      require('unplugin-vue-components/webpack')({
-        /* options */
-      }),
-    ],
+    resolve: {
+      alias: {
+        components: '@/components',
+      },
+    },
   },
+  // configureWebpack: (config) => {
+  //   config.resolve.alias = {
+  //     '@': path.resolve(__dirname, 'src'),
+  //     components: '@/components'
+  //   }
+  // }
+  // 3.配置方式三:
+  // chainWebpack: (config) => {
+  //   config.resolve.alias
+  //     .set('@', path.resolve(__dirname, 'src'))
+  //     .set('components', '@/components')
+  // }
 }
