16c1144359b78397288a9fa6080c24aec9778945
askwuxue <972523419@qq.com>
2021-11-20 00:00:39 +0800

    build: config development BASE_URL

--- src/service/request/config.ts
+++ src/service/request/config.ts
@@ -1,7 +1,7 @@
 // 根据环境判断导出url
 let BASE_URL = ''
 if (process.env.NODE_ENV === 'development') {
-  BASE_URL = 'http://123.207.32.32:8000'
+  BASE_URL = 'http://152.136.185.210:5000'
 } else if (process.env.NODE_ENV === 'production') {
   BASE_URL = 'https://codewhy.org/pord'
 }
