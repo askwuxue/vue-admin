06a12aed63152f37b6554108c298787b9a0c3e0a
askwuxue <972523419@qq.com>
2021-11-20 00:01:42 +0800

    feat: upodate account login logic

--- src/store/login/login.ts
+++ src/store/login/login.ts
@@ -1,6 +1,7 @@
 import { Module } from 'vuex'
 import { IRootState } from '../types'
 import { ILoginState } from './types'
+import { accountLoginRequest } from '../../service/login/login'
 
 const loginModule: Module<ILoginState, IRootState> = {
   namespaced: true,
@@ -13,9 +14,11 @@ const loginModule: Module<ILoginState, IRootState> = {
   getters: {},
   mutations: {},
   actions: {
-    accountLoginAction(context, payload) {
-      console.log('payload: ', payload)
-      console.log('执行accountLoginAction')
+    // 账号密码登录
+    async accountLoginAction(context, payload) {
+      // console.log('payload: ', payload)
+      const res = await accountLoginRequest(payload)
+      console.log('res: ', res)
     },
     phoneLoginAction(context, payload) {
       console.log('payload: ', payload)
