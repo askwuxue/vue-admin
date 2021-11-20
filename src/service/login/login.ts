5db40ce581c570f1e6ddb4c3871a0b553860735b
askwuxue <972523419@qq.com>
2021-11-20 16:38:50 +0800

    feat(request): append request interface

--- src/service/login/login.ts
+++ src/service/login/login.ts
@@ -1,13 +1,29 @@
 import request from '../index'
 import { LoginAPI, IAccount } from './type'
+import { IDataType, ILoginResult } from './type'
 
+// 账号密码请求
 export const accountLoginRequest = (account: IAccount) => {
   const { name, password } = account
-  return request.post({
+  return request.post<IDataType<ILoginResult>>({
     url: LoginAPI.accountLogin,
     data: {
       name,
       password,
     },
+  })
+}
+
+// 用户信息请求
+export const getUserInfo = (id: number) => {
+  return request.get<IDataType>({
+    url: LoginAPI.getUserInfo + id,
+  })
+}
+
+// 获取用户角色菜单信息
+export const getUserRoleMenus = (id: number) => {
+  return request.get<IDataType>({
+    url: `${LoginAPI.getRoleMenu}${id}/menu`,
   })
 }
--- src/service/login/type.ts
+++ src/service/login/type.ts
@@ -1,10 +1,28 @@
 // 登录方式枚举
 export enum LoginAPI {
   accountLogin = '/login',
+  getUserInfo = '/users/',
+  getRoleMenu = '/role/',
 }
+
+// User API 枚举
+export enum UserAPI {}
 
 // account类型
 export interface IAccount {
   name: string
   password: string
+}
+
+// 数据类型
+export interface IDataType<T = any> {
+  code: number
+  data: T
+}
+
+// login返回data类型
+export interface ILoginResult {
+  id: number
+  name: string
+  token: string
 }
