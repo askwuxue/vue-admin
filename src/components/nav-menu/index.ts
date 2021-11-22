327aa64b4a4207bea16b0c72290ee51a3b06443b
askwuxue <972523419@qq.com>
2021-11-22 23:29:59 +0800

    feat(nav-menu): create NavMenu component

---
+++ src/components/nav-menu/index.ts
@@ -0,0 +1,3 @@
+import NavMenu from './src/NavMenu.vue'
+
+export { NavMenu }
---
+++ src/components/nav-menu/src/NavMenu.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="nav-menu">
+    <div class="logo">
+      <!-- 在引号中使用@ 标识符，前面加~ -->
+      <img class="img" src="~@/assets/img/logo.svg" alt="logo" />
+      <span class="title">Vue3+TS</span>
+    </div>
+    <el-menu default-active="2" class="el-menu-vertical">
+      <!-- 循环菜单列表 -->
+      <template v-for="items of roleMenu" :key="items.id">
+        <!-- 一级菜单 -->
+        <template v-if="items.type === 1">
+          <el-sub-menu :index="items.id + ''">
+            <template #title>
+              <el-icon><location /></el-icon>
+              <span>{{ items.name }}</span>
+            </template>
+            <!-- 二级菜单 -->
+            <template v-for="subItem of items.children" :key="subItem.id">
+              <el-menu-item :index="subItem.id + ''">
+                <i v-if="subItem.icon" :class="subItem.icon"></i>
+                <span>{{ subItem.name }}</span>
+              </el-menu-item>
+            </template>
+          </el-sub-menu>
+        </template>
+      </template>
+    </el-menu>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, SetupContext, computed } from 'vue'
+import { useStore } from '@/store/index'
+
+export default defineComponent({
+  name: 'nav-menu',
+  props: {},
+  components: {},
+  setup(props, ctx: SetupContext) {
+    const store = useStore()
+    const roleMenu = computed(() => store.state.login.roleMenuInfo)
+    return {
+      roleMenu,
+    }
+  },
+})
+</script>
+
+<style scoped lang="less">
+.nav-menu {
+  height: 100%;
+  background-color: #001529;
+
+  .logo {
+    display: flex;
+    height: 28px;
+    padding: 12px 10px 8px 10px;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+
+    .img {
+      height: 100%;
+      margin: 0 10px;
+    }
+
+    .title {
+      font-size: 16px;
+      font-weight: 700;
+      color: white;
+    }
+  }
+
+  .el-menu {
+    border-right: none;
+  }
+
+  // 目录
+  .el-submenu {
+    background-color: #001529 !important;
+    // 二级菜单 ( 默认背景 )
+    .el-menu-item {
+      padding-left: 50px !important;
+      background-color: #0c2135 !important;
+    }
+  }
+
+  ::v-deep .el-submenu__title {
+    background-color: #001529 !important;
+  }
+
+  // hover 高亮
+  .el-menu-item:hover {
+    color: #fff !important; // 菜单
+  }
+
+  .el-menu-item.is-active {
+    color: #fff !important;
+    background-color: #0a60bd !important;
+  }
+}
+.el-menu-vertical:not(.el-menu--collapse) {
+  width: 100%;
+  height: calc(100% - 48px);
+}
+</style>
