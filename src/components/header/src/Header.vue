c2af048a586c9eeb30cec06eb8508c0d16abc3e5
askwuxue <972523419@qq.com>
2021-11-26 00:21:30 +0800

    feat: head-info create

--- src/components/header/src/Header.vue
+++ src/components/header/src/Header.vue
@@ -4,15 +4,25 @@
       <el-icon v-if="isFold"><expand /></el-icon>
       <el-icon v-else><fold /></el-icon>
     </i>
+    <!-- 面包屑和用户信息 -->
+    <div class="header-info">
+      <span class="header-crumbs">面包屑</span>
+      <span class="header-userInfo">
+        <nav-icons></nav-icons>
+      </span>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref, SetupContext } from 'vue'
+import navIcons from './nav-icons.vue'
 export default defineComponent({
   name: '',
   props: {},
-  components: {},
+  components: {
+    navIcons,
+  },
   emits: ['foldchange'],
   setup(props, ctx: SetupContext) {
     // 默认折叠
@@ -32,9 +42,17 @@ export default defineComponent({
 
 <style scoped lang="less">
 .nav-header {
+  display: flex;
+  width: 100%;
   .fold-menu {
     font-size: 30px;
     cursor: pointer;
+  }
+  .header-info {
+    display: flex;
+    flex: 1;
+    justify-content: space-between;
+    align-items: center;
   }
 }
 </style>
