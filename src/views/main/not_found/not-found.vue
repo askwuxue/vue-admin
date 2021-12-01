9fe6eba94ecbf652c6671d316464f39c951c3b32
askwuxue <972523419@qq.com>
2021-12-01 23:36:20 +0800

    feat: create 404 page

---
+++ src/views/main/not_found/not-found.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="not_found">not_found</div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'not_found',
+  props: {},
+  components: {},
+})
+</script>
+
+<style scoped lang="less"></style>
