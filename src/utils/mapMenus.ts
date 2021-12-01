5907166db3f98d494ea0a712aa4015496b90e9a5
askwuxue <972523419@qq.com>
2021-12-01 23:40:31 +0800

    feat: append mapPathToMenu function

--- src/utils/mapMenus.ts
+++ src/utils/mapMenus.ts
@@ -1,8 +1,11 @@
 import router from '@/router'
 import { RouteRecordRaw } from 'vue-router'
+
+// 菜单栏中的第一个子菜单
+let firstMenu: any = null
+
+// 映射菜单到路由对象
 export const mapMenusToRoutes = (usesMenus: any) => {
-  // 路由信息
-  // const routes: RouteRecordRaw[] = []
   // 所有路由信息
   const allRoutes: RouteRecordRaw[] = []
   // 加载路由文件
@@ -26,6 +29,11 @@ export const mapMenusToRoutes = (usesMenus: any) => {
         recursiveGetRoute(menu.children)
       }
       const route = allRoutes.find((item) => item.path === menu.url)
+      // 第一个菜单tab
+      if (!firstMenu) {
+        firstMenu = menu
+      }
+      // TODO 下面这段逻辑卸载这个工具类中不合适，应该返回一个routes对象，在外部调用时添加route
       if (route) {
         router.addRoute('main', route)
       }
@@ -33,3 +41,24 @@ export const mapMenusToRoutes = (usesMenus: any) => {
   }
   recursiveGetRoute(usesMenus)
 }
+
+// 地址和菜单ID之前的映射
+export const mapPathToMenu = (usesMenus: any, currentPath: string): any => {
+  for (const menu of usesMenus) {
+    // console.log('menu-----: ', menu.url)
+    // console.log('currentPath-------: ', currentPath)
+    if (menu.type === 1) {
+      const findMenu = mapPathToMenu(menu.children, currentPath)
+      // TODO 如果找到值就返回值，没找到就递归
+      if (findMenu) {
+        return findMenu
+      }
+    }
+    // 菜单模块id和当前路径匹配，返回菜单模块
+    if (menu.type === 2 && menu.url === currentPath) {
+      return menu
+    }
+  }
+}
+
+export { firstMenu }
