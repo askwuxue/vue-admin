38d39ef98badb5ca056d5b14a1215158797b6a2a
askwuxue <972523419@qq.com>
2021-12-07 22:52:48 +0800

    feat: father and child component use v-model

--- src/base-ui/form/src/form.vue
+++ src/base-ui/form/src/form.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <!-- 顶部插槽 -->
+    <div class="header-slot">
+      <slot name="header"></slot>
+    </div>
     <!-- form 表单 -->
     <el-form :label-width="labelWidth">
       <el-row>
@@ -14,8 +18,7 @@
                 <el-input
                   :type="item.type"
                   :placeholder="item.placeholder"
-                  :value="modelValue.name"
-                  @input="updateModelValue"
+                  v-model="formData[`${item.filed}`]"
                 />
               </template>
               <!-- type 属性为select -->
@@ -41,14 +44,19 @@
         </template>
       </el-row>
     </el-form>
+    <!-- 底部插槽 -->
+    <div class="footer-slot">
+      <slot name="footer"></slot>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, SetupContext, PropType } from 'vue'
+import { defineComponent, PropType, reactive, watch } from 'vue'
 import { IFormData } from '../types'
 export default defineComponent({
   name: '',
+  // TODO 表单的配置项抽取到一个对象中
   props: {
     formItems: {
       type: Array as PropType<IFormData[]>,
@@ -70,27 +78,44 @@ export default defineComponent({
         lg: 8,
       }),
     },
-    modelValue: {},
+    modelValue: {
+      type: Object,
+      required: true,
+    },
   },
   components: {},
   emits: ['update:modelValue'],
   setup(props, { emit }) {
-    const newModel = {
-      name: '',
-    }
-    console.log('props: ', props.modelValue)
-    const updateModelValue = (newValue: any) => {
-      // console.log('newValue: ', newValue)
-      // console.log('event: ', event)
-      newModel.name = newValue
-      console.log('newModel: ', newModel)
-      emit('update:modelValue', newModel)
-    }
+    // TODO 使用计算属性进行数据双向绑定时，等同于父组件直接传递了对象数据，计算属性的set在修改对象的某个属性时不会生效，这种方法本质上是引用了同一个对象的引用地址
+    // const formData = computed({
+    //   get: () => props.modelValue,
+    //   set: (newValue) => {
+    //     // console.log('change formData')
+    //     emit('update:modelValue', newValue)
+    //   },
+    // })
+
+    // TODO 使用侦听器的方式，对父组件通过v-model传递的对象数据，不直接修改，子组件维护一份数据，对子组件的数据进行监听，当子组件的input中v-model执行，监听到改变，emit到父组件，实现数据的双向绑定，但是子组件多维护了一份数据。子组件的modelValue无法及时更新？尝试使用一下ref代替reactive
+    const formData = reactive({ ...props.modelValue })
+    watch(
+      formData,
+      (newValue) => {
+        emit('update:modelValue', newValue)
+      },
+      {
+        deep: true,
+      },
+    )
     return {
-      updateModelValue,
+      formData,
     }
   },
 })
 </script>
 
-<style scoped lang="less"></style>
+<style scoped lang="less">
+.footer-slot {
+  float: right;
+  margin-right: 100px;
+}
+</style>
--- src/base-ui/form/types/index.ts
+++ src/base-ui/form/types/index.ts
@@ -1,6 +1,7 @@
 type IFormType = 'input' | 'password' | 'select' | 'datepicker'
 
 export interface IFormData {
+  filed: string
   type: IFormType
   label: string
   rules?: any
