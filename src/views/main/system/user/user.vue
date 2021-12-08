66e2298ff6a4a280133f89311527ce314b3405af
askwuxue <972523419@qq.com>
2021-12-08 22:30:58 +0800

    feat: use packaged table components

--- src/views/main/system/user/user.vue
+++ src/views/main/system/user/user.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="user">
+    <!-- 搜索栏 -->
     <page-search></page-search>
+    <!-- 数据展示栏 -->
+    <wx-table :tableData="userList" :propsData="propsData">
+      <!-- 动态插槽的渲染,指定对应的propName -->
+      <template #status="scope">
+        <el-button>{{ scope.row.enable ? '启用' : '关闭' }}</el-button>
+      </template>
+    </wx-table>
   </div>
 </template>
 
 <script lang="ts">
-import PageSearch from '@/components/page-search'
-import { defineComponent } from 'vue'
-import { useStore } from 'vuex'
+import { PageSearch } from '@/components/page-search'
+import { WxTable } from '@/base-ui/table'
+import { computed, defineComponent } from 'vue'
+// 自定义的useStore
+import { useStore } from '@/store'
 
 export default defineComponent({
   name: 'user',
   components: {
     PageSearch,
+    WxTable,
   },
   setup() {
     const store = useStore()
@@ -23,7 +34,46 @@ export default defineComponent({
         size: 100,
       },
     })
-    return {}
+    const userList = computed(() => store.state.system.userList)
+    // const userList: any[] = []
+    console.log('userList: ', userList)
+    const userListCount = store.state.system.userListCount
+    console.log('userListCount: ', userListCount)
+
+    const propsData = [
+      { prop: 'name', label: '用户名', minWidth: 100, slotName: 'name' },
+      {
+        prop: 'realname',
+        label: '真实姓名',
+        minWidth: 100,
+        slotName: 'realname',
+      },
+      {
+        prop: 'cellphone',
+        label: '手机号码',
+        minWidth: 100,
+        slotName: 'cellphone',
+      },
+      { prop: 'enable', label: '状态', minWidth: 100, slotName: 'status' },
+      {
+        prop: 'createAt',
+        label: '创建时间',
+        minWidth: 100,
+        slotName: 'createAt',
+      },
+      {
+        prop: 'updateAt',
+        label: '更新时间',
+        minWidth: 100,
+        slotName: 'updateAt',
+      },
+    ]
+
+    return {
+      userList,
+      userListCount,
+      propsData,
+    }
   },
 })
 </script>
