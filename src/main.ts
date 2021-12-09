ceed3afe6b39bfe453101b1e6dcea15dd5cc8c1a
askwuxue <972523419@qq.com>
2021-12-09 23:17:25 +0800

    feat: internationalization and date format

--- src/main.ts
+++ src/main.ts
@@ -10,9 +10,15 @@ import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/base.css'
 // 自定义样式
 import '@/assets/css/index.css'
+import format from '@/global/register-formart'
+// 国际化
+// import global from '@/global/global'
 
 const app = createApp(App)
+app.use(format)
 app.use(register)
+// app.use(global)
+
 app.use(store)
 // TODO 动态注册的路由在使用路由之前，否则在路由匹配阶段无法找到对应的路径匹配的路由，导致匹配到404页面
 app.use(setupStore)
