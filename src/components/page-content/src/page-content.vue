213465b75321cbd7eb46c1d59b6a9a366ca5095c
askwuxue <972523419@qq.com>
2021-12-15 23:46:11 +0800

    feat: package page-content component

---
+++ src/components/page-content/src/page-content.vue
@@ -0,0 +1,61 @@
+<template>
+  <div class="page-content">
+    <wx-table v-bind="pageContentConfig" :tableData="userList">
+      <!-- header slot 渲染table的头部 -->
+      <template #headerHandler>
+        <el-button>创建用户</el-button>
+      </template>
+      <!-- 动态插槽的渲染,指定对应的propName -->
+      <template #status="scope">
+        <!-- TODO 根据当前的状态设置不同的button样式 -->
+        <el-button type="primary" size="small" plain>
+          {{ scope.row.enable ? '启用' : '关闭' }}
+        </el-button>
+      </template>
+      <template #createAt="scope">
+        <span>{{ $filter.formatUTCDate(scope.row.createAt) }}</span>
+      </template>
+      <template #updateAt="scope">
+        <span>{{ $filter.formatUTCDate(scope.row.createAt) }}</span>
+      </template>
+
+      <!-- TODO footer slot 渲染table的底部 -->
+    </wx-table>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, SetupContext, computed } from 'vue'
+import { useStore } from '@/store'
+import { WxTable } from '@/base-ui/table'
+import { pageContentConfig } from '@/views/main/system/user/config/content.config'
+
+export default defineComponent({
+  name: '',
+  props: {
+    pageName: {
+      type: String,
+    },
+  },
+  components: {
+    WxTable,
+  },
+  setup(props, ctx: SetupContext) {
+    console.log('props: ', props)
+    const store = useStore()
+    store.dispatch('system/getPageListAction', props.pageName)
+
+    const userList = computed(() => store.state.system.userList)
+    // console.log('userList: ', userList)
+    const userListCount = store.state.system.userListCount
+    // console.log('userListCount: ', userListCount)
+    return {
+      userList,
+      userListCount,
+      pageContentConfig,
+    }
+  },
+})
+</script>
+
+<style scoped lang="less"></style>
---
+++ src/views/main/system/user/config/content.config.ts
@@ -0,0 +1,33 @@
+export const pageContentConfig = {
+  showIndex: true,
+  showSelect: true,
+  title: '用户列表',
+  propsData: [
+    { prop: 'name', label: '用户名', minWidth: 100, slotName: 'name' },
+    {
+      prop: 'realname',
+      label: '真实姓名',
+      minWidth: 100,
+      slotName: 'realname',
+    },
+    {
+      prop: 'cellphone',
+      label: '手机号码',
+      minWidth: 100,
+      slotName: 'cellphone',
+    },
+    { prop: 'enable', label: '状态', minWidth: 100, slotName: 'status' },
+    {
+      prop: 'createAt',
+      label: '创建时间',
+      minWidth: 100,
+      slotName: 'createAt',
+    },
+    {
+      prop: 'updateAt',
+      label: '更新时间',
+      minWidth: 100,
+      slotName: 'updateAt',
+    },
+  ],
+}
