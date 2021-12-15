cf50c92f20d3cf85f2cf464cf723002a8ccd5d18
askwuxue <972523419@qq.com>
2021-12-15 23:44:05 +0800

    fix: update request method name

--- src/service/main/system/system.ts
+++ src/service/main/system/system.ts
@@ -1,9 +1,9 @@
 import request from '@/service'
 import { IDataType } from '../../types'
 
-// 用户信息请求
 // TODO 封装性不够好，能不能像login进行封装
-export const getUserList = ({ url, queryInfo }: any) => {
+// 请求列表数据具有通用性
+export const getList = ({ url, queryInfo }: any) => {
   return request.post<IDataType>({
     url: url,
     data: queryInfo,
