001a30cbee7f1a635cd1af797957354dfeb70b9d
askwuxue <972523419@qq.com>
2021-12-27 23:35:46 +0800

    refactor: update export style

--- src/views/main/system/role/role.vue
+++ src/views/main/system/role/role.vue
@@ -7,7 +7,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import { PageContent } from '@/components/page-content'
+import PageContent from '@/components/page-content'
 import { pageContentConfig } from '@/views/main/system/role/config/content.config'
 
 export default defineComponent({
