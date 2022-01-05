f142c8c37ae3b5883494a2b596ff1054b6019bbd
askwuxue <972523419@qq.com>
2022-01-05 23:06:26 +0800

    feat: get department and role data

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,16 +2,48 @@ import { createStore, useStore as vuexUseStore, Store } from 'vuex'
 import { IRootState, IStoreType } from './types'
 import login from './login/login'
 import system from './main/system/system'
+import { getList } from '@/service/main/system/system'
 
 const store = createStore<IRootState>({
   state() {
     return {
       name: '',
       age: 0,
+      entireDepartment: [],
+      entireRole: [],
     }
   },
-  mutations: {},
-  actions: {},
+  mutations: {
+    // 所有部门
+    updateEntireDepartment(state, departmentList) {
+      state.entireDepartment = departmentList
+    },
+    // 所有角色
+    updateEntireRole(state, roleList) {
+      state.entireRole = roleList
+    },
+  },
+  actions: {
+    // 获取所有部门
+    async getDepartmentAction({ commit }) {
+      const departmentRes = await getList({
+        url: '/department/list',
+        queryInfo: { size: 1000, offset: 0 },
+      })
+      const { list: departmentList } = departmentRes.data
+      commit('updateEntireDepartment', departmentList)
+    },
+
+    // 获取所有的角色
+    async getRoleAction({ commit }) {
+      const roleRes = await getList({
+        url: '/role/list',
+        queryInfo: { size: 1000, offset: 0 },
+      })
+      const { list: roleList } = roleRes.data
+      commit('updateEntireRole', roleList)
+    },
+  },
   modules: {
     login,
     system,
@@ -21,6 +53,8 @@ const store = createStore<IRootState>({
 // 设置store的数据，从localStorage中取值
 export const setupStore = () => {
   store.dispatch('login/solveState')
+  store.dispatch('getDepartmentAction')
+  store.dispatch('getRoleAction')
 }
 
 // 导出useStore函数，对vuex的useStore进行封装
--- src/store/types.ts
+++ src/store/types.ts
@@ -4,6 +4,8 @@ import { ISystemState } from './main/system/types'
 export interface IRootState {
   name: string
   age: number
+  entireDepartment: any[]
+  entireRole: any[]
 }
 
 export interface IRootWithModule {
