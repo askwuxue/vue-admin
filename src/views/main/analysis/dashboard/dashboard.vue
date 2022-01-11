fb53b98182a5e3046213377e95b1c80121031aae
askwuxue <972523419@qq.com>
2022-01-11 23:19:19 +0800

    feat: use packaged page-echarts show data

--- src/views/main/analysis/dashboard/dashboard.vue
+++ src/views/main/analysis/dashboard/dashboard.vue
@@ -3,47 +3,105 @@
     <el-row :gutter="10">
       <el-col :span="7">
         <wx-card title="分类商品数量(饼图)">
-          <base-echarts></base-echarts>
+          <pie-echart :pieData="categoryGoodsCount" />
         </wx-card>
       </el-col>
       <el-col :span="10">
-        <wx-card title="不同城市商品销量"></wx-card>
+        <wx-card title="不同城市商品销量">
+          <map-echart :mapData="addressGoodsSale" />
+        </wx-card>
       </el-col>
       <el-col :span="7">
-        <wx-card title="分类商品数量(玫瑰图)"></wx-card>
+        <wx-card title="分类商品数量(玫瑰图)">
+          <rose-echart :roseData="categoryGoodsCount" />
+        </wx-card>
       </el-col>
     </el-row>
 
     <el-row :gutter="10" class="content-row">
       <el-col :span="12">
-        <wx-card title="分类商品的销量"></wx-card>
+        <wx-card title="分类商品的销量">
+          <line-echart v-bind="categoryGoodsSale" />
+        </wx-card>
       </el-col>
       <el-col :span="12">
-        <wx-card title="分类商品的收藏"></wx-card>
+        <wx-card title="分类商品的收藏">
+          <bar-echart v-bind="categoryGoodsFavor" />
+        </wx-card>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { computed, defineComponent } from 'vue'
 import WxCard from '@/base-ui/card'
-import BaseEcharts from '@/base-ui/base-Echarts'
 import { useStore } from '@/store'
+import {
+  PieEchart,
+  RoseEchart,
+  LineEchart,
+  BarEchart,
+  MapEchart,
+} from '@/components/page-echarts'
 
 export default defineComponent({
   name: 'dashboard',
   components: {
     WxCard,
-    BaseEcharts,
+    PieEchart,
+    RoseEchart,
+    LineEchart,
+    BarEchart,
+    MapEchart,
   },
   setup() {
     const store = useStore()
-    const getData = () => {
-      store.dispatch('dashboard/getDashboardDataAction')
+    store.dispatch('dashboard/getDashboardDataAction')
+    const categoryGoodsCount = computed(() => {
+      return store.state.dashboard.categoryGoodsCount.map((item) => ({
+        name: item.name,
+        value: item.goodsCount,
+      }))
+    })
+
+    const categoryGoodsFavor = computed(() => {
+      const xLabels: string[] = []
+      const values: any[] = []
+      const categoryGoodsFavor = store.state.dashboard.categoryGoodsFavor
+      for (const item of categoryGoodsFavor) {
+        xLabels.push(item.name)
+        values.push(item.goodsFavor)
+      }
+      return { xLabels, values }
+    })
+
+    const categoryGoodsSale = computed(() => {
+      const xLabels: string[] = []
+      const values: any[] = []
+      const categoryGoodsSale = store.state.dashboard.categoryGoodsSale
+      for (const item of categoryGoodsSale) {
+        xLabels.push(item.name)
+        values.push(item.goodsCount ?? 1)
+      }
+      return {
+        xLabels,
+        values,
+      }
+    })
+
+    const addressGoodsSale = computed(() => {
+      return store.state.dashboard.addressGoodsSale.map((item: any) => {
+        return { name: item.address, value: item.count }
+      })
+    })
+
+    return {
+      categoryGoodsCount,
+      categoryGoodsSale,
+      categoryGoodsFavor,
+      addressGoodsSale,
     }
-    getData()
-    return {}
   },
 })
 </script>
